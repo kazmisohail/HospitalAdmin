db6d9c4f62d5b1c34fe781a2a9a19ef4a0fdc2f1
Hatim <hatimhaiderali786@gmail.com>
2024-05-19 23:53:45 +0500

    login styles

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,19 @@
 function logIn() {
     window.location.href = "./dashboard.html";
 }
+
+function togglePasswordVisibility() {
+    const passwordField = document.getElementById('password');
+    const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordField.setAttribute('type', type);
+    const eyeIcon = document.getElementById('eye-icon');
+    if (eyeIcon.classList.contains('fa-regular')) {
+        eyeIcon.classList.remove('fa-regular');
+        eyeIcon.classList.add('fa-solid');
+    }
+
+    else {
+        eyeIcon.classList.add('fa-regular');
+        eyeIcon.classList.remove('fa-solid');
+    }
+}
